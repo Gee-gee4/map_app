c1d101652064d453ec4358f167edc68fc867ba2e
Gee <gladysmbuthia324@gmail.com>
2025-05-03 23:04:05 +0300

    constructed a menu item category model

---
+++ lib/Model/menu_data.dart
@@ -0,0 +1,69 @@
+import 'package:map_app/Model/menu_item.dart';
+
+final List<MenuItem> menuCategories = [
+  MenuItem(
+      name: 'Grilled Chicken',
+      category: 'Meals',
+      description: 'Will get you full',
+      price: 12.99),
+  MenuItem(
+      name: 'Beef Burger',
+      category: 'Meals',
+      description: 'Will get you full',
+      price: 12.99),
+  MenuItem(
+      name: 'Pasta',
+      category: 'Meals',
+      description: 'Will get you full',
+      price: 12.99),
+  MenuItem(
+      name: 'Coffee',
+      category: 'Drinks',
+      description: 'Will get you full',
+      price: 12.99),
+  MenuItem(
+      name: 'Tea',
+      category: 'Drinks',
+      description: 'Will get you full',
+      price: 12.99),
+  MenuItem(
+      name: 'Juice',
+      category: 'Drinks',
+      description: 'Will get you full',
+      price: 12.99),
+  MenuItem(
+      name: 'Ice Cream',
+      category: 'Desert',
+      description: 'Will get you full',
+      price: 12.99),
+  MenuItem(
+      name: 'Chocolate Cake',
+      category: 'Desert',
+      description: 'Will get you full',
+      price: 12.99),
+  MenuItem(
+      name: 'Cheesecake',
+      category: 'Desert',
+      description: 'Will get you full',
+      price: 12.99),
+];
+
+/*
+Meals:
+Grilled Chicken
+Beef Burger
+Pasta
+Fish
+
+Drinks:
+Mohito
+Coffee
+Tea
+Water
+
+Deserts:
+Chocolate Cake
+Cheesecake
+Apple Pie
+Ice Cream
+ */
